ee0d9aa2800864360fac7134765bcd75e9372a3c
Simon Norman <01secn@gmail.com>
2019-01-23 16:45:46 +0000

    Split input students into several methods to properly abstract it

--- directory.rb
+++ directory.rb
@@ -1,75 +1,80 @@
 
+@students = [] # an empty array accessible to all methods
 
 def interactive_menu
-  students = []
-    
   loop do
+    print_menu
+    
+    process(gets.chomp)
+  end
+end
+
+def print_menu
     puts "1. Input the students"
     puts "2. Show the students"
     puts "9. Exit" # 9 because we'll be adding more items
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean, try again"
+  end
+end
+
+def input_students
+  catch :finish do
+    loop do
     
-    selection = gets.chomp
-    
-    case selection
-      when "1"
-        students = input_students
-      when "2"
-        print_header
-        print(students)
-        print_footer(students)
-      when "9"
-        exit # this will cause the program to terminate
-      else
-        puts "I don't know what you meant, try again"
+      new_student = get_blank_student
+      get_blank_student.each do |field, value|
+        new_student[field] = get_user_input(field)
+      end
+        
+      @students << new_student
+      print_count
     end
   end
 end
 
-def input_students 
-  students = []
-    
-  while true do
-  
-    puts "Please enter the name of a student"
-    puts "To exit, enter 'exit'"
-    name = user_input
-  
-    if name == 'exit'
-      break
-    end
-    
-    puts "Please enter the cohort of the student"
-    puts "To exit, enter 'exit"
-    cohort = user_input
-    
-    if cohort == 'exit'
-      break
-    end
+def get_blank_student
+  { :name => "", :cohort => "" }
+end
+
+def get_user_input(field)
+  puts "Please enter the #{field} of the student"
+  puts "To exit, enter 'exit'"
       
-    students << {name: name, cohort: cohort.to_sym}
-    print_count(students)
-  end
-  
-  students
-end
-
-def user_input
   while true do
     input = gets.sub("\n", "")
     
     if input == ""
       puts "This is an invalid response, please try again."
+    elsif input == "exit"
+      throw :finish
     else
       return input
     end
   end
 end
 
-def print_count(students)
-  if students.count == 1
-    puts "Now we have #{students.count} student"
+def show_students
+  print_header
+  print_students
+  print_footer
+end
+
+def print_count
+  if @students.count == 1
+    puts "Now we have #{@students.count} student"
   else
-    puts "Now we have #{students.count} students"
+    puts "Now we have #{@students.count} students"
   end
 end
 
@@ -82,28 +87,27 @@ def print_aligned(text)
   puts text.center(100)
 end
 
-def group_students_by_cohort(students)
+def group_students_by_cohort
   students_by_cohort = Hash.new { |hash, key| hash[key] = [] }
     
-  students.map { |student| 
+  @students.map { |student| 
     students_by_cohort[student[:cohort].to_sym].push(student[:name])
   }
   
   return students_by_cohort
 end
 
-def print(students)
-  students_by_cohort = group_students_by_cohort(students)
+def print_students
+  students_by_cohort = group_students_by_cohort
     
   students_by_cohort.each do |cohort, student_names|
     names_as_string = student_names.join(", ")
     print_aligned("#{cohort}: #{names_as_string}")
   end
-
 end
 
-def print_footer(students)
-   print_aligned("Overall, we have #{students.count} great students")
+def print_footer
+   print_aligned("Overall, we have #{@students.count} great students")
 end
 
 interactive_menu
