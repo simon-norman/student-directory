cd09752a354a034f7a4486d3a9da051cc1ddbec5
Simon Norman <01secn@gmail.com>
2019-01-23 15:53:04 +0000

    Added interative menu

--- directory.rb
+++ directory.rb
@@ -1,29 +1,68 @@
 
+
+def interactive_menu
+  students = []
+    
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" # 9 because we'll be adding more items
+    
+    selection = gets.chomp
+    
+    case selection
+      when "1"
+        students = input_students
+      when "2"
+        print_header
+        print(students)
+        print_footer(students)
+      when "9"
+        exit # this will cause the program to terminate
+      else
+        puts "I don't know what you meant, try again"
+    end
+  end
+end
+
 def input_students 
   students = []
-  puts "Please enter the name of students and their cohort
-      separated by a comma and space (e.g. 'Tom, November'"
-  puts "To finish, just hit return twice"
     
   while true do
   
-    input = gets.sub("\n", "")
-
-    if input.empty?
+    puts "Please enter the name of a student"
+    puts "To exit, enter 'exit'"
+    name = user_input
+  
+    if name == 'exit'
       break
-    else
-      student = input.split(", ")
-      
-      if student[0] && student[1]
-        students << {name: student[0], cohort: student[1].to_sym}
-        print_count(students)
-      else
-        puts "You have not entered all the required info, please try again."
-      end
     end
+    
+    puts "Please enter the cohort of the student"
+    puts "To exit, enter 'exit"
+    cohort = user_input
+    
+    if cohort == 'exit'
+      break
+    end
+      
+    students << {name: name, cohort: cohort.to_sym}
+    print_count(students)
   end
   
   students
+end
+
+def user_input
+  while true do
+    input = gets.sub("\n", "")
+    
+    if input == ""
+      puts "This is an invalid response, please try again."
+    else
+      return input
+    end
+  end
 end
 
 def print_count(students)
@@ -43,24 +82,28 @@ def print_aligned(text)
   puts text.center(100)
 end
 
-def print(students)
+def group_students_by_cohort(students)
   students_by_cohort = Hash.new { |hash, key| hash[key] = [] }
-  
+    
   students.map { |student| 
     students_by_cohort[student[:cohort].to_sym].push(student[:name])
   }
   
+  return students_by_cohort
+end
+
+def print(students)
+  students_by_cohort = group_students_by_cohort(students)
+    
   students_by_cohort.each do |cohort, student_names|
     names_as_string = student_names.join(", ")
     print_aligned("#{cohort}: #{names_as_string}")
   end
+
 end
 
 def print_footer(students)
    print_aligned("Overall, we have #{students.count} great students")
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+interactive_menu
